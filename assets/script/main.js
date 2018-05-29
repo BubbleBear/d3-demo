96948ee65d4a2af67171c6ef81e5bf30cd6e3dd3
雪霁 <wang.haiguang@puscene.com>
2018-05-29 17:16:06 +0800

    init

---
+++ assets/script/main.js
@@ -0,0 +1,56 @@
+let width = 960;
+let height = 600;
+let radius = 30;
+
+const svg = d3.select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+const events = {
+    drag: {
+    },
+};
+
+function initSimulation() {
+    return d3.forceSimulation()
+        .force('centering', d3.forceCenter(width / 2, height / 2))
+        .force('collision', d3.forceCollide()
+            .radius(radius)
+            .strength(0.7))
+        .force('many-body', d3.forceManyBody()
+            .strength(-10)  // negative to push away, positive to draw each other
+            .theta(0.9)     // dunno wtf is this
+            .distanceMin(radius)
+            .distanceMax(radius * 5))
+}
+
+function drawNodes(nodes) {
+    return svg.append('g')
+        .classed('nodes', true)
+        .selectAll('.avatar')
+        .data(nodes)
+        .enter()
+        .append('circle')
+        .classed('avatar', true)
+        .attr('r', radius)
+        // .attr('fill', d => {})
+}
+
+async function start() {
+    const data = await d3.json('relation.json');
+
+    const simulation = initSimulation();
+
+    let nodes = drawNodes(data.nodes);
+
+    simulation
+        .nodes(data.nodes)
+        .on('tick', () => {
+            nodes
+                .attr('cx', d => d.x)
+                .attr('cy', d => d.y)
+        })
+}
+
+start();
