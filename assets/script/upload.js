62014f29052f0388a1fd4a415d2ea3b920c68d46
雪霁 <wang.haiguang@puscene.com>
2018-06-05 15:43:18 +0800

    single relation

--- assets/script/upload.js
+++ assets/script/upload.js
@@ -1,15 +1,38 @@
 export default (ctx) => {
     const fileInput = document.querySelector('#avatar_upload');
-
     const preview = d3.select('.preview');
-
     const name = document.querySelector('input.name');
-
-    const select = document.querySelector('select.relations');
-    const index = select.selectedIndex;
-    const relationWith = select[index];
+    const relationsSelect = document.querySelector('select.relations');
+    const relationsInput = document.querySelector('input.relations');
 
     let blob;
+
+    function appendOptions() {
+        d3.selectAll('select.relations')
+            .selectAll('option.relations')
+            .remove();
+            
+        d3.selectAll('select.relations')
+            .selectAll('option.relations')
+            .data(ctx.data.nodes)
+            .enter()
+            .append('option')
+            .classed('relations', true)
+            .attr('value', v => v.id)
+            .html(v => v.id);
+    }
+
+    function getRelations() {
+        const index = relationsSelect.selectedIndex;
+        const relationsTarget= relationsSelect[index].value;
+        const relationsName = relationsInput.value;
+        return { target: relationsTarget, relation: relationsName }
+    }
+
+    d3.select('select.relations')
+        .on('mousedown', () => {
+            appendOptions();
+        })
 
     d3.select('input[type=file]')
         .on('change', () => {
@@ -20,18 +43,23 @@ export default (ctx) => {
 
     d3.select('input[type=submit]')
         .on('click', () => {
+            const relations = getRelations();
+
             ctx.data.nodes = ctx.data.nodes.concat({
                 id: name.value,
                 url: blob,
             });
 
+            ctx.data.edges = ctx.data.edges.concat({
+                source: name.value,
+                target: relations.target,
+                relation: relations.relation,
+            })
+
             ctx.drawSvg(ctx.data);
             ctx.startSimulation(ctx.data)
             ctx.registerEvents();
-        })
-
-    d3.select('input.relations[type=text]')
-        .on('change', () => {
-            console.log(relationWith)
+            
+            ctx.simulation.alpha(0.9).restart();
         })
 };
